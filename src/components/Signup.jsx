04edaf02df78bc72d75a9a437d8088ca0214fbb3
Nayem khan <nayem178548@gmail.com>
2025-12-07 06:44:34 +0000

    final form action done

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,6 @@
 import {isEmail,isNotEmpty,isEqualToOtherValue,hasMinLength} from '../util/validation.js';
 import { useActionState } from 'react';
-export default function Signup() {
-  
+
   function signUpAction(prev,formData){
 const email=formData.get('email');
 const password=formData.get('password');
@@ -45,7 +44,15 @@ errors.push('please select atleast one acqusition channel');
 }
 
 if(errors.length>0){
-return{errors};
+return{errors,enteredValues:{
+  email,
+  password,
+  confirmPassword,
+  firstName,
+  lastName,
+  aquisitonChannel,
+  terms
+}};
 
 }
 
@@ -53,6 +60,10 @@ return {errors:null}
 
 
 }
+
+
+export default function Signup() {
+  
 
 const [formState,formAction]=useActionState(signUpAction, {
   errors:null,
@@ -67,13 +78,13 @@ const [formState,formAction]=useActionState(signUpAction, {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email}/>
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" defaultValue={formState.enteredValues?.password} />
         </div>
 
         <div className="control">
@@ -82,7 +93,8 @@ const [formState,formAction]=useActionState(signUpAction, {
             id="confirm-password"
             type="password"
             name="confirm-password"
-          />
+defaultValue={formState.enteredValues?.confirmPassword}
+/>
         </div>
       </div>
 
@@ -91,18 +103,18 @@ const [formState,formAction]=useActionState(signUpAction, {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" defaultValue={formState.enteredValues?.firstName} />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" defaultValue={formState.enteredValues?.lastName} />
         </div>
       </div>
 
       <div className="control">
-        <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <label htmlFor="role">What best describes your role?</label>
+        <select id="role" name="role" defaultValue={formState.enteredValues?.role}>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -119,6 +131,7 @@ const [formState,formAction]=useActionState(signUpAction, {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.aquisitonChannel.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -129,20 +142,21 @@ const [formState,formAction]=useActionState(signUpAction, {
             id="friend"
             name="acquisition"
             value="friend"
+             defaultChecked={formState.enteredValues?.aquisitonChannel.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input type="checkbox" id="other" name="acquisition" value="other"   defaultChecked={formState.enteredValues?.aquisitonChannel.includes('other')}/>
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input type="checkbox" id="terms-and-conditions" name="terms"  defaultChecked={formState.enteredValues?.terms}/>I
+          agree to the terms and condition?
         </label>
       </div>
 
